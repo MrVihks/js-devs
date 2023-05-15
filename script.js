a9cd7306e7a2c54c9f3c6e504be7d56b60478d38
Vinicius Henrique <79711257+MrVihks@users.noreply.github.com>
2023-05-15 12:25:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const menuBtn = document.querySelector('#menu');
 const menu = document.querySelectorAll('#navbar')
+const presentation = document.querySelector('#desc');
+
+const names =  ["Vinicius", "Victor", "Xaropinho", "Luiz Sene"]; 
+let i = 0;
+setInterval(()=>{  
+    if (i < names.length){ 
+      presentation.style.webkitAnimationPlayState = "initial";
+      presentation.innerHTML = names[i];
+      i++;
+    }else{
+      i = 0;
+    }
+  },3500);
 
 document.addEventListener('click', (e)=>{
     const el = e.target;
